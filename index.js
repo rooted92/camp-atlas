d501102e38f0ea1214dddd45406fed9a0b23bb2b
Pedro Castaneda <petecast76@gmail.com>
2024-01-28 03:02:15 -0800

    feat: campground create

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ db.once('open', () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
@@ -24,6 +25,22 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show.ejs', { campground });
 });
 
 app.listen(port, () => {
