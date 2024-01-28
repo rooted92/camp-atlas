5f3f55d2f1fa0c7a6e272079d7f0c2bc0f8da23f
Pedro Castaneda <petecast76@gmail.com>
2024-01-28 01:29:18 -0800

    feat: campground model setup

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
 
+// Models
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/camp-atlas');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected')
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground(
+        {
+            title: 'My Backyard',
+            price: 0,
+            description: 'Cheap camping!'
+        }
+    );
+    await camp.save(); 
+    res.send(camp);
 });
 
 app.listen(port, () => {
