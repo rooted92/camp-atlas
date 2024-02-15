559f93acb8b4196b9179df40a262e0c000598a60
Pedro Castaneda <petecast76@gmail.com>
2024-02-15 01:01:02 -0800

    feat: setup static assets

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const ExpressError = require('./utilities/ExpressError');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 // Routes
 const campgroundRoutes = require('./routes/campgrounds.js');
@@ -25,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(cookieParser('thisismysecret'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
