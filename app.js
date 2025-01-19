7d9ebdee81b2fec2be14cb4dbe5a455b40b51b82
Pedro Castaneda <petecast76@gmail.com>
2025-01-19 02:16:10 -0800

    feat: setup mong db connection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/camp-atlas', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
 
 const app = express();
 
@@ -9,6 +22,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (require, response) => {
    response.render('home.ejs');
+});
+
+app.get('/makecampground', async (require, response) => {
+    const camp = new Campground({name: 'My Backyard', price: 'Free', description: 'A large backyard', location: 'My House'});
+    await camp.save();
+    response.render('makecampground.ejs');
 });
 
 app.listen(3000, () => {
