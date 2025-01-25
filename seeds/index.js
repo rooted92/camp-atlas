241c990c6028781575be123d75e9b99111e9670a
Pedro Castaneda <petecast76@gmail.com>
2025-01-24 16:34:48 -0800

    feat: key names in model, import descriptors and places

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camp-atlas', {
@@ -13,18 +14,18 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            name: `${cities[random1000].city} Camp`,
-            description: 'A large backyard',
-            price: Math.floor(Math.random() * 20) + 10
+            title: `${sample(descriptors)} ${sample(places)}`,
         });
         await camp.save();
     }
 }
 
-seedDB();
+seedDB().then(() => { mongoose.connection.close(); });
