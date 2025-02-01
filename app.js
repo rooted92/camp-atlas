9fb2a6f1060f8b70515e787cc564fdeda606c832
Pedro Castaneda <petecast76@gmail.com>
2025-02-01 03:21:19 -0800

    feat: use moran middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const morgan = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/camp-atlas');
 
@@ -16,9 +17,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 
 app.get('/', (require, response) => {
     response.render('home.ejs');
