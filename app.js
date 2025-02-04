ae90e32eada9fa0b0c46092a267a70e23a271680
Pedro Castaneda <petecast76@gmail.com>
2025-02-04 09:00:11 -0800

    feat: install ejs-mate, implement layout boilerplate, add layout to current pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/camp-atlas');
 
@@ -18,7 +19,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use(morgan('dev'));
+app.engine('ejs', ejsMate);
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
