0f055ceb11bf2e2d773dbd215fe42f8db53e45dd
Pedro Castaneda <petecast76@gmail.com>
2025-02-07 09:10:21 -0800

    feat: reseed db with new model properties

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://unsplash.com/collections/10489597/camping',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cum voluptates quaerat amet autem at quis labore ad numquam modi deserunt distinctio excepturi quisquam, ipsa dignissimos quae vero asperiores repellat!',
+            price,
         });
         await camp.save();
     }
