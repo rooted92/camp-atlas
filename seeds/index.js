b57c584821436923076ad9ff979ce7f1d40fec45
Pedro Castaneda <petecast76@gmail.com>
2025-02-07 09:30:32 -0800

    feat: install axios, fix: use unsplash access key in function to get random photo

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const axios = require('axios');
 
 mongoose.connect('mongodb://localhost:27017/camp-atlas', {
     useNewUrlParser: true,
@@ -16,6 +17,20 @@ db.once('open', () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function getRandomImage() {
+    try {
+      const response = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'l4jBZU6_2zq0cuR4as_XKLDmiEARRILgl9oJpKFf6Ls',
+          collections: 1114848,
+        },
+      })
+      return response.data.urls.small
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
@@ -24,7 +39,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://unsplash.com/collections/10489597/camping',
+            image: await getRandomImage(),
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cum voluptates quaerat amet autem at quis labore ad numquam modi deserunt distinctio excepturi quisquam, ipsa dignissimos quae vero asperiores repellat!',
             price,
         });
